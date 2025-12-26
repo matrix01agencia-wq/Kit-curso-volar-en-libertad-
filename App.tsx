235be3d4b610da211e296f570650d2cfba6f3932
matrix01agencia-wq <matrix01agencia@gmail.com>
2025-12-25 22:16:41 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -158,10 +158,10 @@ const App: React.FC = () => {
           </div>
           
           <h1 className="text-4xl md:text-8xl font-montserrat font-black leading-[0.9] mb-8 uppercase tracking-tighter">
-            EL ARSENAL DIGITAL DE <br />
+            EL ARSENAL DIGITAL DEFINITIVO PARA CONSTRUIR <br />
             <span className="line-through text-red-600 opacity-40">$1,780 USD</span> <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#FF00FF] via-white to-[#00FF00]">
-              ESTÁ A PUNTO DE MORIR.
+              UN IMPERIO SIN SER UN EXPERTO 
             </span>
           </h1>
           
